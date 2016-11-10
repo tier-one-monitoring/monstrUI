491ab4293cd7f6c8fa8502ef53395a37ee93659d
IgorPelevanuk <gavelock@gmail.com>
2016-11-10 15:54:24 +0300

    UPDATE: Templates added, deploy script modified

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-cp plugins dist bootstrap /var/www/lcgsens01o.jinr.ru/static/.
+# Copy all static files to apache directory
+cp -rf plugins dist bootstrap /var/www/lcgsens01o.jinr.ru/static/.
+
+# Copy templates to Django directory
+cp -rf templates /opt/monstrWeb/.
+
